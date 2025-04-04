8a322add087e301c0af540f3bc2fded9b8e74217
Connor McColl <connormccoll@gmail.com>
2025-04-04 07:44:25 -0500

    :sunglasses: swe, sswe content

--- src/app/resume/senior-software-engineer.tsx
+++ src/app/resume/senior-software-engineer.tsx
@@ -1,7 +1,9 @@
 export const seniorSoftwareEngineerContent = [
-    "Frontend sub-team lead, regularly pair program with team members and review merge/pull requests.",
-    "Delivered a robust and maintainable workflow-oriented UI from POC through presenting product release demo for the first release for an AWS-run IaC (Infrastructure as Code) provisioning service. The service has a high impact, reducing time from order to deployment from weeks to days, and enabling subscription-based revenue and upgrades.",
-    "Design additional UIs with wireframes in Figma and define Task Flows and User Flows with Miro for early product feedback.",
-    "Integrated with IDAM (Identity and Access Management) and added session management to the UI in accordance with security and privacy requirements.",
-    "Shaved weeks off development time by implementing a generated client and mock API."
+    "Front end developer leading UI sub-team of 4 engineers on AWS run IaC (Infrastructure as Code) service. Developing workflow-oriented UIs for multistep processes to provision cloud application packages using CloudFormation and other provisioners. The project is a top business priority to achieve a SaaS model by reducing duration from purchase order to fulfillment from weeks to days, unlocking subscription-based revenue, and enabling over the air upgrades. Often lead product discussions for defining user requirements, feature prioritization, and UI design.",
+    "Lead developing of stateless UIs featuring multistep workflows, data grids, and RBAC (Role Based Access Control).",
+    "Spearhead user requirements definition and generate user flows in Miro. Design UIs following the UX design process with an emphasis on creating detailed prototypes in Figma.",
+    "Documented best practices for standardizing use of Microsoft Copilot generative AI in UI development.",
+    "Achieved significant first release milestone after one year of development and presented release demo. Reached the team goal of introducing new features on a quarterly release schedule and on track for consumer adoption targets.",
+    "Reduced development time by 4 weeks utilizing a generated Angular client and mock Node.js API.",
+    "Hackathon finalist team for creating an OpenAI LLM chatbot for debugging execution logs."
 ]
--- src/app/resume/software-engineer.ts
+++ src/app/resume/software-engineer.ts
@@ -1,9 +1,7 @@
 export const softwareEngineerContent = [
-    "Front end developer leading UI sub-team of 4 engineers on AWS run IaC (Infrastructure as Code) service. Developing workflow-oriented UIs for multistep processes to provision cloud application packages using CloudFormation and other provisioners. The project is a top business priority to achieve a SaaS model by reducing duration from purchase order to fulfillment from weeks to days, unlocking subscription-based revenue, and enabling over the air upgrades. Often lead product discussions for defining user requirements, feature prioritization, and UI design.",
-    "Lead developing of stateless UIs featuring multistep workflows, data grids, and RBAC (Role Based Access Control).",
-    "Spearhead user requirements definition and generate user flows in Miro. Design UIs following the UX design process with an emphasis on creating detailed prototypes in Figma.",
-    "Documented best practices for standardizing use of Microsoft Copilot generative AI in UI development.",
-    "Achieved significant first release milestone after one year of development and presented release demo. Reached the team goal of introducing new features on a quarterly release schedule and on track for consumer adoption targets.",
-    "Reduced development time by 4 weeks utilizing a generated Angular client and mock Node.js API.",
-    "Hackathon finalist team for creating an OpenAI LLM chatbot for debugging execution logs."
+    "Full-stack software developer in the IaaS (Infrastructure as a Service) and IaC space. Contributed high quality features to both Angular front end and Node.js backend API, learned AWS technologies, thoroughly unit tested and BDD tested code. Manager and project leader for two summer interns. ",
+    "Implemented internationalization into UI, enabling global use.",
+    "Added IAM (Identity and Access Management) integration, session management, met security and privacy requirements.",
+    "Managed rotational program member’s project adapting Kubernetes run resource scheduling application to GPU sharing.",
+    "Resolved defects and transferred ownership of legacy application. Led testing best practices and created a unit test training process, maintaining coverage over 90%."
 ];
