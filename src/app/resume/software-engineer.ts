de9d01cb52e52a8c7b29f2395ef529962d094b1f
Connor McColl <connormccoll@gmail.com>
2025-04-04 07:36:10 -0500

    :fire: new photo

--- src/app/resume/software-engineer.ts
+++ src/app/resume/software-engineer.ts
@@ -1,6 +1,9 @@
 export const softwareEngineerContent = [
-    "Full-stack software development on 3 applications in the IaaS (Infrastructure as a Service) and IaC space. Contributed high quality features to both frontend and backend API using Angular and Node JS.",
-    "Additionally, integrated internationalization into the UI, enabling global use of an application which automates a step in a technologists’ workflow on a per exam basis.",
-    "Manager and project leader for two summer interns. Mentor for rotational program members.",
-    "Resolved defects and transferred out ownership of applications. Led testing best practices and created a unit testing training process, boosting overall coverage from 85% to over 90%.",
+    "Front end developer leading UI sub-team of 4 engineers on AWS run IaC (Infrastructure as Code) service. Developing workflow-oriented UIs for multistep processes to provision cloud application packages using CloudFormation and other provisioners. The project is a top business priority to achieve a SaaS model by reducing duration from purchase order to fulfillment from weeks to days, unlocking subscription-based revenue, and enabling over the air upgrades. Often lead product discussions for defining user requirements, feature prioritization, and UI design.",
+    "Lead developing of stateless UIs featuring multistep workflows, data grids, and RBAC (Role Based Access Control).",
+    "Spearhead user requirements definition and generate user flows in Miro. Design UIs following the UX design process with an emphasis on creating detailed prototypes in Figma.",
+    "Documented best practices for standardizing use of Microsoft Copilot generative AI in UI development.",
+    "Achieved significant first release milestone after one year of development and presented release demo. Reached the team goal of introducing new features on a quarterly release schedule and on track for consumer adoption targets.",
+    "Reduced development time by 4 weeks utilizing a generated Angular client and mock Node.js API.",
+    "Hackathon finalist team for creating an OpenAI LLM chatbot for debugging execution logs."
 ];
